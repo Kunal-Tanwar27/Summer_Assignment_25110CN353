e71969e16637c86497dd1861093b3b861583f575
Kunal-Tanwar27 <25110cn353@glbitm.ac.in>
2026-06-05 19:02:38 +0530

    Day5 assignment is completed

---
+++ Day5/Perfectno.java
@@ -0,0 +1,31 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class Perfectno {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println(" enter the number to be checked ");
+    int num = sc.nextInt();
+
+    if (num <= 0) {
+      System.out.println(" invalid entry");
+      sc.close();
+      return;
+    }
+
+    int sum = 0;
+    for (int i = 1; i <= num / 2; i++) {
+      if (num % i == 0) {
+        sum = sum + i;
+      }
+    }
+    if (sum == num) {
+      System.out.println(num + " is a Perfect number");
+    } else {
+      System.out.println(num + " is not a Perfect number");
+    }
+    sc.close();
+  }
+}
